6c291809301e1d4fb4911559737c2c28dbbfb7df
Kyle Moore <moorekyle2022@fau.edu>
2023-10-31 04:39:45 -0400

    Step 10: Update the collection view's layout      - Lab 3 Completed!

--- lab-tunley/AlbumsViewController.swift
+++ lab-tunley/AlbumsViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import Nuke
 
-class AlbumsViewController: UIViewController {
+class AlbumsViewController: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // the number of items shown should be the number of albums we have.
+        albums.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Get a collection view cell (based in the identifier you set in storyboard) and cast it to our custom AlbumCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
+
+        // Use the indexPath.item to index into the albums array to get the corresponding album
+        let album = albums[indexPath.item]
+
+        // Get the artwork image url
+        let imageUrl = album.artworkUrl100
+
+        // Set the image on the image view of the cell
+        Nuke.loadImage(with: imageUrl, into: cell.albumImageView)
+
+        return cell
+    }
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var albums: [Album] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
 
         // Do any additional setup after loading the view.
         // Create a search URL for fetching albums (`entity=album`)
@@ -38,12 +64,38 @@ class AlbumsViewController: UIViewController {
                 // Try to parse the response into our custom model
                 let response = try decoder.decode(AlbumSearchResponse.self, from: data)
                 let albums = response.results
-                self?.albums = albums
+                
+                DispatchQueue.main.async {
+                    self?.albums = albums
+                    self?.collectionView.reloadData()
+                }
                 // print(albums)
             } catch {
                 print(error.localizedDescription)
             }
         }
+        
+        // Get a reference to the collection view's layout
+        // We want to dynamically size the cells for the available space and desired number of columns.
+        // NOTE: This collection view scrolls vertically, but collection views can alternatively scroll horizontally.
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+        // The minimum spacing between adjacent cells (left / right, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumInteritemSpacing = 4
+
+        // The minimum spacing between adjacent cells (top / bottom, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumLineSpacing = 4
+
+        // Set this to however many columns you want to show in the collection.
+        let numberOfColumns: CGFloat = 3
+
+        // Calculate the width each cell need to be to fit the number of columns, taking into account the spacing between cells.
+        let width = (collectionView.bounds.width - layout.minimumInteritemSpacing * (numberOfColumns - 1)) / numberOfColumns
+
+        // Set the size that each tem/cell should display at
+        layout.itemSize = CGSize(width: width, height: width)
 
         // Initiate the network request
         task.resume()
